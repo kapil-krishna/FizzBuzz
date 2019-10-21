740c3c72b8bc5c1cdcfd9c36603a5a6dd5fa49db
Kapil Krishna <Kapil.Krishna@softwire.com>
2019-10-21 16:00:06 +0100

    bruteforce fizzBuzzBang

--- src/kapil/fizzbuzz/Main.java
+++ src/kapil/fizzbuzz/Main.java
@@ -3,17 +3,31 @@ package kapil.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 101; i++) {
+        int max = 100;
+        for (int i = 1; i <= max; i++) {
             int fizz = i % 3;
             int buzz = i % 5;
-            if (fizz == 0 && buzz == 0) {
+            int bang = i % 7;
+            if (fizz == 0 && buzz == 0 && bang == 0) {
+                System.out.println("FizzBuzzBang");
+            }
+            else if (fizz == 0 && buzz == 0) {
                 System.out.println("FizzBuzz");
+            }
+            else if (buzz == 0 && bang == 0) {
+                System.out.println("BuzzBang");
+            }
+            else if (fizz == 0 && bang == 0) {
+                System.out.println("FizzBang");
             }
             else if (fizz == 0) {
                 System.out.println("Fizz");
             }
             else if (buzz == 0) {
                 System.out.println("Buzz");
+            }
+            else if (bang == 0) {
+                System.out.println("Bang");
             }
             else {
                 System.out.println(i);
