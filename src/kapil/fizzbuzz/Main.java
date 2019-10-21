60f313bad75480a36b036318d9470c3d7189bf3f
Kapil Krishna <Kapil.Krishna@softwire.com>
2019-10-21 14:38:06 +0100

    first push

---
+++ src/kapil/fizzbuzz/Main.java
@@ -0,0 +1,15 @@
+package kapil.fizzbuzz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if (i % 3 === 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 === 0) {
+
+            }
+        }
+    }
+}
