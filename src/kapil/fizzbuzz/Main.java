9554f89d78c821dcc8504ac364539a46c7c17351
Kapil Krishna <Kapil.Krishna@softwire.com>
2019-10-21 16:33:08 +0100

    fizzbuzzbang cleaned

--- src/kapil/fizzbuzz/Main.java
+++ src/kapil/fizzbuzz/Main.java
@@ -3,36 +3,26 @@ package kapil.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-        int max = 100;
+        int max = 105;
         for (int i = 1; i <= max; i++) {
             int fizz = i % 3;
             int buzz = i % 5;
             int bang = i % 7;
-            String = ""
-            if (fizz == 0 && buzz == 0 && bang == 0) {
-                System.out.println("FizzBuzzBang");
+            String word = "";
+
+            if (fizz == 0) {
+                word += "Fizz";
             }
-            else if (fizz == 0 && buzz == 0) {
-                System.out.println("FizzBuzz");
+            if (buzz == 0) {
+                word += "Buzz";
             }
-            else if (buzz == 0 && bang == 0) {
-                System.out.println("BuzzBang");
+            if (bang == 0) {
+                word += "Bang";
             }
-            else if (fizz == 0 && bang == 0) {
-                System.out.println("FizzBang");
+            else if (fizz != 0 && buzz != 0 && bang != 0) {
+                word = Integer.toString(i);
             }
-            else if (fizz == 0) {
-                System.out.println("Fizz");
-            }
-            else if (buzz == 0) {
-                System.out.println("Buzz");
-            }
-            else if (bang == 0) {
-                System.out.println("Bang");
-            }
-            else {
-                System.out.println(i);
-            }
+            System.out.println(word);
         }
     }
 }
