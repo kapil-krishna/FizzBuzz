959a06f2d78dddfae16f677e16d54e0a01fc9c00
Kapil Krishna <Kapil.Krishna@softwire.com>
2019-10-21 14:50:55 +0100

    "second commit"

--- src/kapil/fizzbuzz/Main.java
+++ src/kapil/fizzbuzz/Main.java
@@ -3,12 +3,20 @@ package kapil.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 101; i++) {
-            if (i % 3 === 0) {
+        for (int i = 1; i < 101; i++) {
+            int fizz = i % 3;
+            int buzz = i % 5;
+            if (fizz == 0 && buzz == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (fizz == 0) {
                 System.out.println("Fizz");
             }
-            else if (i % 5 === 0) {
-
+            else if (buzz == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
             }
         }
     }
